871123adb44f386409cf80733473bbead759a027
Mo Zhang <928951156@qq.com>
2022-11-27 21:48:56 +1100

    Revert "Pure BLE scanner all Kotlin"
    This reverts commit 3289b81b08353a89519221014004e21189e5a37b.

---
+++ app/src/main/java/com/nenoff/connecttoarduinoble/RemoteControl.java
@@ -0,0 +1,34 @@
+/*
+ * (c) Matey Nenov (https://www.thinker-talk.com)
+ *
+ * Licensed under Creative Commons: By Attribution 3.0
+ * http://creativecommons.org/licenses/by/3.0/
+ *
+ */
+
+package com.nenoff.connecttoarduinoble;
+
+public class RemoteControl {
+    private final static byte LED_COMMAND = 0x4;
+
+    private final static byte VALUE_OFF = 0x0;
+    private final static byte VALUE_ON = (byte)0xFF;
+
+    private BLEController bleController;
+
+    public RemoteControl(BLEController bleController) {
+        this.bleController = bleController;
+    }
+
+    private byte [] createControlWord(byte type, byte ... args) {
+        byte [] command = new byte[args.length];
+        for(int i = 0; i < args.length; i++)
+            command[i] = args[i];
+        return command;
+    }
+
+    public void switchLED(boolean on) {
+        this.bleController.sendData(createControlWord(LED_COMMAND, on?VALUE_ON:VALUE_OFF));
+    }
+
+}
